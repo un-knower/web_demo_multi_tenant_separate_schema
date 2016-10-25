782b1f19fad03f43814abfd420232ae6095001d6
rg <uuuzombie@gmail.com>
2016-10-25 08:31:24 +0800

    add kafka

---
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package com.sky.demo.web_demo_multi_tenant_separate_schema.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rg on 25/10/2016.
+ */
+public class KafkaConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+}
---
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/kafka/producer/KafkaProducer.java
@@ -0,0 +1,14 @@
+package com.sky.demo.web_demo_multi_tenant_separate_schema.kafka.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rg on 25/10/2016.
+ */
+public class KafkaProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+
+
+}
