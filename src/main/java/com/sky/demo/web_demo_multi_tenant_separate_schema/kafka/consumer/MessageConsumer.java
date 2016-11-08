5d0b94cbf71e3655c973f4dc5f625d5ad5476855
rg <uuuzombie@gmail.com>
2016-11-08 18:03:23 +0800

    add consumer

--- src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/kafka/consumer/MessageConsumer.java
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/kafka/consumer/MessageConsumer.java
@@ -1,7 +1,16 @@
 package com.sky.demo.web_demo_multi_tenant_separate_schema.kafka.consumer;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.sky.demo.web_demo_multi_tenant_separate_schema.util.AppConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by rg on 25/10/2016.
@@ -11,6 +20,23 @@ public class MessageConsumer {
     private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
 
+    private static Properties props;
+    static {
+        props = new Properties();
+        props.put("bootstrap.servers", AppConfig.getItem("kafka.bootstrap.servers"));
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", AppConfig.getItem("kafka.enable.auto.commit")); //是否自动commit
+        props.put("auto.commit.interval.ms", "1000");   //定时commit的周期
+        props.put("session.timeout.ms", "30000");       //consumer活性超时时间
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+    }
+
+    private KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+    private int CONSUMER_SIZE = 100;
+
     // singleton
     private static MessageConsumer INSTANCE = new MessageConsumer();
 
@@ -18,6 +44,53 @@ public class MessageConsumer {
 
     public static MessageConsumer getInstance() {
         return INSTANCE;
+    }
+
+
+
+
+    public List<ConsumerRecord<String, String>> consume(List<String> topics, int size) {
+        List<ConsumerRecord<String, String>> result = Lists.newArrayList();
+
+        int consumedSize = 0;
+        try {
+            consumer.subscribe(topics);
+
+            do {
+                int offset = (size - consumedSize) > CONSUMER_SIZE ? CONSUMER_SIZE : (size - consumedSize);
+
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(offset);
+                for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                    result.add(consumerRecord);
+                }
+
+                consumedSize += offset;
+
+            } while (consumedSize < size);
+        } catch (Exception e) {
+            logger.error("poll message failed", e);
+        } finally {
+            consumer.close();
+        }
+
+        return result;
+    }
+
+
+    /**
+     * properties : enable.auto.commit = false
+     * @param topics
+     * @param size
+     * @return
+     */
+    public List<String> consumeByManulCommit(List<String> topics, int size) {
+
+
+        List<String> result = null;
+        consumer.subscribe(topics);
+
+
+        return result;
     }
 
 }
--- src/test/java/com/sky/demo/web_demo_multi_tenant_separate_schema/kafka/consumer/MessageConsumerTest.java
+++ src/test/java/com/sky/demo/web_demo_multi_tenant_separate_schema/kafka/consumer/MessageConsumerTest.java
@@ -1,10 +1,26 @@
 package com.sky.demo.web_demo_multi_tenant_separate_schema.kafka.consumer;
 
+import com.google.common.collect.Lists;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by user on 16/11/7.
  */
 public class MessageConsumerTest {
 
 
+    @Test
+    public void test_consume() {
+        List<String> topics = Lists.newArrayList("test");
+        int size = 3;
+
+        MessageConsumer consumer = MessageConsumer.getInstance();
+        List<ConsumerRecord<String, String>> result = consumer.consume(topics, size);
+
+        System.out.println(result);
+    }
 
 }
