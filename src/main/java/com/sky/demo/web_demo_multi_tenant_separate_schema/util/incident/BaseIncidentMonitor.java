a26181622f6f96436204fe33faf5a67f25957b9d
rg <uuuzombie@gmail.com>
2016-11-17 13:46:55 +0800

    add incident monitor

---
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/util/incident/BaseIncidentMonitor.java
@@ -0,0 +1,8 @@
+package com.sky.demo.web_demo_multi_tenant_separate_schema.util.incident;
+
+/**
+ * Created by user on 16/11/11.
+ */
+public abstract class BaseIncidentMonitor implements Runnable {
+
+}
