889c19ca6e1b279471402ae9ce9a81859da0103c
rg <uuuzombie@gmail.com>
2017-01-14 11:08:53 +0800

    add lock

--- src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/redis/service/StringRedisServiceImpl.java
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/redis/service/StringRedisServiceImpl.java
@@ -8,6 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by user on 17/1/12.
@@ -16,6 +19,11 @@ import javax.annotation.Resource;
 public class StringRedisServiceImpl implements StringRedisService {
 
     private static final Logger logger = LoggerFactory.getLogger(StringRedisServiceImpl.class);
+
+    private Lock lock = new ReentrantLock();
+
+    private Condition condition = lock.newCondition();
+
 
     @Resource
     private StringRedisTemplateWrapper stringRedisTemplateWrapper;
@@ -29,8 +37,18 @@ public class StringRedisServiceImpl implements StringRedisService {
         Preconditions.checkNotNull(key, "key is null!");
 
         logger.info("string redis template set key : {}", key);
-        stringRedisTemplateWrapper.set(key, value);
+
+        try {
+            lock.lock();
+            stringRedisTemplateWrapper.set(key, value);
 //        valueRedisDao.set(key, value);
+
+//            condition.signalAll();
+        } catch (Exception e) {
+            logger.error("redis set error", e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
@@ -38,8 +56,20 @@ public class StringRedisServiceImpl implements StringRedisService {
         Preconditions.checkNotNull(key, "key is null!");
 
         logger.info("string redis template get key : {}", key);
-        return stringRedisTemplateWrapper.get(key);
-//        return (String)valueRedisDao.get(key);
+        String result = null;
+        try {
+            lock.lock();
+
+            result = stringRedisTemplateWrapper.get(key);
+//        String result = (String)valueRedisDao.get(key);
+
+        } catch (Exception e) {
+            logger.error("redis get error", e);
+        } finally {
+            lock.unlock();
+        }
+
+        return result;
     }
 
     @Override
@@ -47,7 +77,16 @@ public class StringRedisServiceImpl implements StringRedisService {
         Preconditions.checkNotNull(key, "key is null!");
 
         logger.info("string redis template delete key : {}", key);
-        stringRedisTemplateWrapper.delete(key);
+
+        try {
+            lock.lock();
+
+            stringRedisTemplateWrapper.delete(key);
 //        valueRedisDao.delete(key);
+        } catch (Exception e) {
+            logger.error("redis delete error", e);
+        } finally {
+            lock.unlock();
+        }
     }
 }
