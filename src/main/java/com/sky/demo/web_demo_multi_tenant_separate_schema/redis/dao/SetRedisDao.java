1dcf78d75b520b35f968398886c2624481da9cf2
rg <uuuzombie@gmail.com>
2017-02-09 10:22:46 +0800

    update redis dao

---
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/redis/dao/SetRedisDao.java
@@ -0,0 +1,7 @@
+package com.sky.demo.web_demo_multi_tenant_separate_schema.redis.dao;
+
+/**
+ * Created by user on 17/2/8.
+ */
+public interface SetRedisDao {
+}
---
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_schema/redis/dao/SetRedisDaoImpl.java
@@ -0,0 +1,3 @@
+package com.sky.demo.web_demo_multi_tenant_separate_schema.redis.dao;
+
+
