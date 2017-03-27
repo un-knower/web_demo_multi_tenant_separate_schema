54d818013a07840e242aa00086680cedbef0c2e2
rg <uuuzombie@gmail.com>
2017-03-27 10:52:34 +0800

    update add tenant sh

--- depoly/tenant/add_tenant.sh
+++ depoly/tenant/add_tenant.sh
@@ -31,7 +31,7 @@ CreateSchema()
 
     if [ ${#SCHEMA_NAME} -le 3 ]; then
         echo "input tenant schema name too short, need more than 3 character"
-        return 1;
+        return 1
     elif echo "${SCHEMA_NAME}" | grep -q '^[a-z][a-z0-9]\+$' ; then
         echo -e "\n ==> create schema : ${SCHEMA_NAME} ..."
 #   psql -h ${DB_IP} -p ${DB_PORT} -U ${DB_USERNAME} -d ${DB_NAME} < ${FILE}
@@ -45,10 +45,10 @@ psql "host=${DB_IP} port=${DB_PORT} user=${DB_USERNAME} password=${DB_PASSWD} db
     \set ON_ERROR_STOP TRUE
     create schema ${SCHEMA_NAME};
 EOF
-        return 0;
+        return 0
     else
         echo "invalid input!"
-        return 1;
+        return 1
     fi
 
 }
